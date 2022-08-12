43aaaf04edc008b90ef551bdd4a482797263bad8
Tkrishnaveni <45869263+Tkrishnaveni@users.noreply.github.com>
2022-08-12 17:28:26 +0530

    Add files via upload

---
+++ dyn.py
@@ -0,0 +1,31 @@
+{
+"_meta": {
+"hostvars": {
+"localhost": {
+"ansible_user": "root",
+"ansible_ssh_private_key_file": "/root/.ssh/id_rsa.pub",
+"ansible_port": 22
+},
+"192.168.1.101": {
+"ansible_user": "root",
+"ansible_ssh_private_key_file": "/home/.ssh/id_rsa",
+"ansible_port": 22
+},
+"192.168.1.190": {
+"ansible_user": "root",
+"ansible_ssh_private_key_file": "/home/.ssh/id_rsa",
+"ansible_port": 22
+}
+}
+},
+"ubuntubox": [ 
+
+"127.0.0.1"
+],
+"vagrantbox": [
+"192.168.1.101"
+],
+"vagrantbox": [
+"192.168.1.190"
+] 
+}
